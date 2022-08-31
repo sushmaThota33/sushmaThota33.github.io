214bf8e76c4c1a096c2336b1162b2b9ee28d2352
sushmaThota33 <70374283+sushmaThota33@users.noreply.github.com>
2022-08-31 17:28:25 +0530

    Components needed to run the website
    Components needed to run the website https://sushmathota33.github.io/ in local

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Screen1 from './Components/Screen1';
+
+function App() {
+  return (
+    <div className="App">
+      <Screen1/>
+    </div>
+  );
+}
+
+export default App;
